b3109b9036afd3799348a27203a0f3d694e41166
luisrpfigueiredo <luis.rpfigueiredo@gmail.com>
2022-04-07 16:26:11 +0100

    feat: ARCH-001: add missing queries (#3)
    * added mostly working setup. items and itemprops and bids being created with success, bid custom props for some reason are broken
    
    * finished ensuring both files can be parsed, in theory should work for any file. Minor refactor and code cleaning
    
    * generated visualization graph
    
    * added queries
    
    * remove useless async declaration
    
    Co-authored-by: Luis Figueiredo <luisfigueiredo@MacBook-Pro.home>

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,4 +1,4 @@
-import { idArg, nonNull, objectType } from 'nexus'
+import { idArg, nonNull, objectType, stringArg } from 'nexus'
 
 export const Query = objectType({
   name: 'Query',
@@ -14,6 +14,126 @@ export const Query = objectType({
       args: { id: nonNull(idArg()) },
       resolve: (_parent, args, context) => {
         return context.prisma.organization.findUnique({
+          where: { id: args.id },
+        })
+      },
+    })
+    t.nonNull.list.nonNull.field('projects', {
+      type: 'Project',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.project.findMany()
+      },
+    })
+    t.nonNull.list.nonNull.field('organizationProjects', {
+      type: 'Project',
+      args: { organizationId: nonNull(stringArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.project.findMany({
+          where: { organizationId: args.organizationId },
+        })
+      },
+    })
+    t.field('project', {
+      type: 'Project',
+      args: { id: nonNull(idArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.project.findUnique({
+          where: { id: args.id },
+        })
+      },
+    })
+    t.field('items', {
+      type: 'Item',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.item.findMany()
+      },
+    })
+    t.field('projectItems', {
+      type: 'Item',
+      args: { projectId: nonNull(stringArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.item.findMany({
+          where: { projectId: args.projectId },
+        })
+      },
+    })
+    t.field('item', {
+      type: 'Item',
+      args: { id: nonNull(idArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.item.findUnique({
+          where: { id: args.id },
+        })
+      },
+    })
+    t.field('customItemProperties', {
+      type: 'CustomItemProperty',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.customItemProperty.findMany()
+      },
+    })
+    t.field('itemCustomItemProperties', {
+      type: 'Item',
+      args: { itemId: nonNull(stringArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.customItemProperty.findMany({
+          where: { itemId: args.itemId },
+        })
+      },
+    })
+    t.field('customItemProperty', {
+      type: 'Item',
+      args: { id: nonNull(idArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.customItemProperty.findUnique({
+          where: { id: args.id },
+        })
+      },
+    })
+    t.field('bids', {
+      type: 'Bid',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.bid.findMany()
+      },
+    })
+    t.field('itemBids', {
+      type: 'Bid',
+      args: { itemId: nonNull(stringArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.bid.findMany({
+          where: { itemId: args.itemId },
+        })
+      },
+    })
+    t.field('bid', {
+      type: 'Bid',
+      args: { id: nonNull(idArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.bid.findUnique({
+          where: { id: args.id },
+        })
+      },
+    })
+    t.field('customBidProperties', {
+      type: 'Bid',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.customBidProperty.findMany()
+      },
+    })
+    t.field('bidCustomBidProperties', {
+      type: 'Bid',
+      args: { bidId: nonNull(stringArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.customBidProperty.findMany({
+          where: { bidId: args.bidId },
+        })
+      },
+    })
+    t.field('customItemProperty', {
+      type: 'CustomBidProperty',
+      args: { id: nonNull(idArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.customBidProperty.findUnique({
           where: { id: args.id },
         })
       },
